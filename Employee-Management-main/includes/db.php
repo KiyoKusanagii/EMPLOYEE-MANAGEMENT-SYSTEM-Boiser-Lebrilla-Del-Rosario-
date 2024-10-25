9b0a6b25478187ac412bf5e2f3626c87a4239c39
KiyoKusanagii <charlesemmanueld02@gmail.com>
2024-10-26 00:06:41 +0800

    Update db.php

--- Employee-Management-main/includes/db.php
+++ Employee-Management-main/includes/db.php
@@ -1,10 +1,10 @@
 <?php
-$host = 'localhost';
-$db = 'management';
-$user = 'root'; // or your db username
-$pass = ''; // or your db password
+$host = 'sql113.infinityfree.com';
+$db = 'if0_37588923_employee_management';
+$user = 'if0_37588923'; // or your db username
+$pass = 'emplyeesytm24'; // or your db password
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli ("sql113.infinityfree.com', 'if0_37588923', 'emplyeesytm24', if0_37588923_employee_management");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
